d624d3bcb3124587bc2173d80564ee92d7c17e15
MarionetkaX <sergey07061999@gmail.com>
2022-02-02 16:21:10 +0300

    add: root and user certificate

--- http/main.py
+++ http/main.py
@@ -1,13 +1,50 @@
-from fastapi import FastAPI
+import os, json, shutil
+from typing import Optional
+from fastapi import \
+    FastAPI,\
+    Request,\
+    File,\
+    UploadFile, Path, Query
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.post('/certificate/root/{name}')
+async def certificates_root(name: str, file: UploadFile = File(...)):
+    root_certificate = os.path.join('certificate', 'root', f'{name}')
+    with open(root_certificate, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    os.system(f'cat {root_certificate} | /scripts/root | touch 1.txt')
+
+
+@app.post('/certificate/user/{name}')
+async def certificates_root(name: str, file: UploadFile = File(...)):
+    user_certificate = os.path.join('./', 'certificate', 'user', f'{name}')
+    with open(user_certificate, 'wb') as buffer:
+        content = await file.read()
+        buffer.write(content)
+        buffer.close()
+    os.system(f'cat {user_certificate} | /scripts/my')
+
+
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    t = 'Hi'
+    return t
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get('/license/{serial_number}')
+async def certificates(serial_number: str):
+    os.system(f'cpconfig -license -set {serial_number}')
+
+
+@app.post('/signer')
+async def certificates():
+    return {'signer'}
+
+
+@app.post('/verify')
+async def certificates():
+    return {'verify'}
+
