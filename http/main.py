95a9e13181dedb8ba56f58a79e3003bd9ca1f3dc
MarionetkaX <sergey07061999@gmail.com>
2022-02-04 15:30:07 +0300

    fix: installation by post requests of the root and user certificate

--- http/main.py
+++ http/main.py
@@ -4,28 +4,35 @@ from fastapi import \
     FastAPI,\
     Request,\
     File,\
-    UploadFile, Path, Query
+    UploadFile, Header
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 app = FastAPI()
 
 
-@app.post('/certificate/root/{name}')
-async def certificates_root(name: str, file: UploadFile = File(...)):
-    root_certificate = os.path.join('certificate', 'root', f'{name}')
-    with open(root_certificate, 'wb') as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    os.system(f'cat {root_certificate} | /scripts/root | touch 1.txt')
+@app.post('/items/')
+async def create_item(item: Item):
+    return item
 
 
-@app.post('/certificate/user/{name}')
-async def certificates_root(name: str, file: UploadFile = File(...)):
-    user_certificate = os.path.join('./', 'certificate', 'user', f'{name}')
-    with open(user_certificate, 'wb') as buffer:
-        content = await file.read()
-        buffer.write(content)
-        buffer.close()
-    os.system(f'cat {user_certificate} | /scripts/my')
+@app.post('/files/')
+async def create_file(file: bytes = File(...)):
+    return {'file_size': print(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    return {'file_size': file.filename}
 
 
 @app.get('/')
@@ -34,9 +41,30 @@ async def root():
     return t
 
 
-@app.get('/license/{serial_number}')
+@app.post('/certificate/root')
+async def certificates_root(file: UploadFile = File(...)):
+    root_certificate = os.path.join('certificate', 'root', file.filename)
+    with open(root_certificate, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    os.system(f'cat {root_certificate} | /scripts/root')
+    return {'status': 'Root certificate set'}
+
+
+@app.post('/certificate/user')
+async def certificates_root(file: UploadFile = File(...)):
+    user_certificate = os.path.join('./', 'certificate', 'user', file.filename)
+    with open(user_certificate, 'wb') as buffer:
+        content = await file.read()
+        buffer.write(content)
+        buffer.close()
+    os.system(f'cat {user_certificate} | /scripts/my')
+    return {'status': 'Root certificate set'}
+
+
+@app.post('/license')
 async def certificates(serial_number: str):
     os.system(f'cpconfig -license -set {serial_number}')
+    return {'status': 'Serial number license set'}
 
 
 @app.post('/signer')
