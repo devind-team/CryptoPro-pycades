dcc59dd064e727f1f702746d6108d3f766306182
MarionetkaX <sergey07061999@gmail.com>
2022-02-11 14:48:27 +0300

    fix: amendments

--- http/main.py
+++ http/main.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import pycades
 import base64
+import json
+import filecmp
 from fastapi import \
     FastAPI,\
     File,\
@@ -10,28 +12,40 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-"""General signature data"""
-store = pycades.Store()
-store.Open(pycades.CADESCOM_CONTAINER_STORE, pycades.CAPICOM_MY_STORE, pycades.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED)
-certs = store.Certificates
-assert (certs.Count != 0), "Certificates with private key not found"
 
-signer = pycades.Signer()
-signer.Certificate = certs.Item(1)
-signer.CheckCertificate = True
+async def store_data():
+    store = pycades.Store()
+    store.Open(pycades.CADESCOM_CONTAINER_STORE, pycades.CAPICOM_MY_STORE, pycades.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED)
+    certs = store.Certificates
+    assert (certs.Count != 0), "Certificates with private key not found"
+    return certs
+
+
+async def signature_data():
+    certificate = await store_data()
+    signer = pycades.Signer()
+    signer.Certificate = certificate.Item(1)
+    signer.CheckCertificate = True
+    return signer
+
+
+async def write_file(save_file, data_file):
+    with open(save_file, 'wb') as buffer:
+        shutil.copyfileobj(data_file.file, buffer)
 
 
 @app.get('/')
 async def main_page():
-    docs = 'http://127.0.0.1:8000/docs#/'
+    docs = 'http://127.0.0.1:8001/docs#/'
     return f'Link to go to interactive API documents {docs}'
 
 
 @app.get('/certificate')
 async def data_certificates():
+    certificate = await store_data()
     ret = []
-    for i in range(1, certs.Count+1):
-        cert = certs.Item(i)
+    for i in range(1, certificate.Count+1):
+        cert = certificate.Item(i)
         issuer = cert.IssuerName.split(',')
         subject = cert.SubjectName.split(',')
         cert_info = {
@@ -65,41 +79,38 @@ async def data_certificates():
             'hasPrivateKey': cert.HasPrivateKey()
         }
         ret.append(cert_info)
-    return JSONResponse(content=ret)
+    return JSONResponse(content={'certificate': ret})
 
 
 @app.post('/certificate/root')
 async def root_certificates(file: UploadFile = File(...)):
-    # root_certificate = os.path.join('certificate', 'root', file.filename)
-    # with open(root_certificate, 'wb') as buffer:
-    #    shutil.copyfileobj(file.file, buffer)
-    t = await file.read()
-    os.system(f'{t} | /scripts/root')
-    return JSONResponse(content={'status': 'Installing a root certificate'})
+    root_certificate = os.path.join('tmp', file.filename)
+    await write_file(root_certificate, file)
+    os.system(f'cat {root_certificate}  | /scripts/root')
+    os.remove(root_certificate)
+    return JSONResponse(content={'status': 'Root certificate installed'})
 
 
 @app.post('/certificate/user')
 async def user_certificates(file: UploadFile = File(...)):
-    user_certificate = os.path.join('./', 'certificate', 'user', file.filename)
-    with open(user_certificate, 'wb') as buffer:
-        content = await file.read()
-        buffer.write(content)
-        buffer.close()
+    user_certificate = os.path.join('tmp', file.filename)
+    await write_file(user_certificate, file)
     os.system(f'cat {user_certificate} | /scripts/my')
-    return JSONResponse(content={'status': 'Installing a user certificate'})
+    os.remove(user_certificate)
+    return JSONResponse(content={'status': 'User certificate installed'})
 
 
 @app.post('/license')
 async def license_number(serial_number: str):
     os.system(f'cpconfig -license -set {serial_number}')
-    return JSONResponse(content={'status': 'Installing a serial number license'})
+    return JSONResponse(content={'status': 'Serial number license installed'})
 
 
 @app.post('/signer')
 async def signer_file(file: UploadFile = File(...)):
     signed_data = pycades.SignedData()
     signed_data.Content = base64.b64encode(await file.read()).decode()
-    signature = signed_data.SignCades(signer, pycades.CADESCOM_CADES_BES)
+    signature = signed_data.SignCades(await signature_data(), pycades.CADESCOM_CADES_BES)
     return JSONResponse(content={'signedContent': signature,
                                  'filename': f'{file.filename}.sig'
                                  })
@@ -107,15 +118,11 @@ async def signer_file(file: UploadFile = File(...)):
 
 @app.post('/unsigner')
 async def unsigner_file(file: UploadFile = File(...)):
-    contents = (await file.read()).decode()
     _signedData = pycades.SignedData()
-    _signedData.VerifyCades(contents, pycades.CADESCOM_CADES_BES)
-    unsignature = _signedData.Content
-    return JSONResponse(content={'unsignedContent': unsignature,
+    _signedData.VerifyCades((await file.read()).decode(), pycades.CADESCOM_CADES_BES)
+    return JSONResponse(content={'unsignedContent': _signedData.Content,
                                  'filename': f"{file.filename.replace('.sig', '')}"
                                  })
 
 
-@app.post('/verify')
-async def verified_file(file, file_signer: UploadFile = File(...)):
-    return {'verify'}
+
